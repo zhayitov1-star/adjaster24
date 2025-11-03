94eacf47498e4a7ef1ceacff847c9b5bf3a237be
Zafar Hayitov <zhayitov1@gmail.com>
2025-11-03 16:01:55 +0500

    Initial commit

---
+++ asset/js/main.js
@@ -0,0 +1,21 @@
+window.onscroll = function () {
+  myFunction();
+};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+    var heroSwiper = new Swiper(".hero-swiper", {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
